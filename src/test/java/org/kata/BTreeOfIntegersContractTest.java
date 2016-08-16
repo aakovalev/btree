9dce8ca94b4b92759311b5f8db1a49cf164dad0d
Alexey Kovalev <Alexey.Kovalev@emc.com>
2016-08-16 11:49:51 +0300

    made refactoring of tests to improve their readability

--- src/test/java/org/kata/BTreeOfIntegersContractTest.java
+++ src/test/java/org/kata/BTreeOfIntegersContractTest.java
@@ -2,6 +2,9 @@ package org.kata;
 
 import org.junit.Test;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
@@ -11,22 +14,23 @@ import static org.junit.Assert.assertTrue;
 public class BTreeOfIntegersContractTest {
     @Test
     public void canAddOneKeyToEmptyTree() throws Exception {
-        int key = 1234;
-        BTreeOfIntegers tree = new BTreeOfIntegers(3);
-        tree.insert(key);
+        int branchingFactor = 3;
+        BTreeOfIntegers tree = new BTreeOfIntegers(branchingFactor);
+        insertKeysIntoTree(keys(1234), tree);
 
-        assertTrue("Tree should contain just added key", tree.contains(key));
+        assertTrue("Tree should contain just added key", tree.contains(1234));
         assertFalse("Tree should not contain key that is not yet added",
                 tree.contains(4567));
     }
 
     @Test
     public void canRemoveKeyAddedPreviously() throws Exception {
-        int key = 1234;
+        int testKey = 1234;
         BTreeOfIntegers tree = new BTreeOfIntegers(3);
-        tree.remove(key);
+        insertKeysIntoTree(keys(1234), tree);
+        tree.remove(testKey);
 
-        assertFalse("Tree should not contain removed key", tree.contains(key));
+        assertFalse("Tree should not contain removed testKey", tree.contains(testKey));
     }
 
     @Test
@@ -35,23 +39,18 @@ public class BTreeOfIntegersContractTest {
     {
         int branchFactor = 2;
         WhiteBoxTestableBTreeOnIntegers tree =
-                new WhiteBoxTestableBTreeOnIntegers(2);
-        tree.insert(1234);
-        tree.insert(5678);
-        tree.insert(9012);
-        // next operation should cause node split
-        tree.insert(3456);
+                new WhiteBoxTestableBTreeOnIntegers(branchFactor);
+        insertKeysIntoTree(keys(1234, 5678, 9012, 3456), tree);
 
         BTreeNode expectedRootNode = new BTreeNode(branchFactor);
-        expectedRootNode.insertNonFull(5678);
+        insertKeysIntoNode(keys(5678), expectedRootNode);
         assertThat(tree.getRoot(), is(expectedRootNode));
 
         BTreeNode expectedLeftNode = new BTreeNode(branchFactor);
-        expectedLeftNode.insertNonFull(1234);
-        expectedLeftNode.insertNonFull(3456);
+        insertKeysIntoNode(keys(1234, 3456), expectedLeftNode);
 
         BTreeNode expectedRightNode = new BTreeNode(branchFactor);
-        expectedRightNode.insertNonFull(9012);
+        insertKeysIntoNode(keys(9012), expectedRightNode);
 
         assertThat(tree.getRoot().getChildNodes(),
                 contains(expectedLeftNode, expectedRightNode));
@@ -64,27 +63,33 @@ public class BTreeOfIntegersContractTest {
         int branchingFactor = 2;
         WhiteBoxTestableBTreeOnIntegers tree =
                 new WhiteBoxTestableBTreeOnIntegers(branchingFactor);
-        tree.insert(1234);
-        tree.insert(5678);
-        tree.insert(9012);
-        tree.insert(3456);
-        tree.insert(4010);
-        tree.insert(4020);
+        insertKeysIntoTree(keys(1234, 5678, 9012, 3456, 4010, 4020), tree);
 
         BTreeNode first = new BTreeNode(branchingFactor);
-        first.insertNonFull(1234);
+        insertKeysIntoNode(keys(1234), first);
 
         BTreeNode second = new BTreeNode(branchingFactor);
-        second.insertNonFull(4010);
-        second.insertNonFull(4020);
+        insertKeysIntoNode(keys(4010, 4020), second);
 
         BTreeNode third = new BTreeNode(branchingFactor);
-        third.insertNonFull(9012);
+        insertKeysIntoNode(keys(9012), third);
 
         BTreeNode root = tree.getRoot();
 
         assertThat(root.getKeys(), contains(3456, 5678));
         assertThat(root.getChildNodes(), contains(first, second, third));
+    }
+
+    private void insertKeysIntoTree(List<Integer> keys, BTreeOfIntegers tree) {
+        keys.forEach(tree::insert);
+    }
+
+    private void insertKeysIntoNode(List<Integer> keys, BTreeNode node) {
+        keys.forEach(node::insertNonFull);
+    }
+
+    private List<Integer> keys(Integer... key) {
+        return asList(key);
     }
 
     private static class WhiteBoxTestableBTreeOnIntegers
@@ -93,7 +98,6 @@ public class BTreeOfIntegersContractTest {
         public WhiteBoxTestableBTreeOnIntegers(int branchingFactor) {
             super(branchingFactor);
         }
-
         public BTreeNode getRoot() {
             return root;
         }
